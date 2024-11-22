952323954230d3673a6ab9aadd9340facfe664bb
surtecweb <surtecwebsite@gmail.com>
2024-11-22 21:12:31 +0530

    Add files via upload

--- js/plugins.js
+++ js/plugins.js
@@ -499,44 +499,65 @@ $(function() {
   // Get the messages div.
   var formMessages = $('.form-messege');
 
+  emailjs.init('GYWwk-TMkQNkWCX_r');
+
+  $('#contact-form').submit(function (e) {
+    e.preventDefault(); // Prevent default form submission
+
+    // Use Email.js to send form data
+    emailjs.sendForm('service_3hohnk5', 'template_z92odcj', this)
+        .then(function (response) {
+            // Success feedback
+            $('.form-messege').removeClass('error').addClass('success');
+            $('.form-messege').text('Email sent successfully.');
+
+            // Clear the form inputs
+            $('#contact-form')[0].reset();
+        }, function (error) {
+            // Error feedback
+            $('.form-messege').removeClass('success').addClass('error');
+            $('.form-messege').text('Oops! An error occurred: ' + error.text);
+        });
+});
+
   // Set up an event listener for the contact form.
-  $(form).submit(function(e) {
-    // Stop the browser from submitting the form.
-    e.preventDefault();
+  // $(form).submit(function(e) {
+  //   // Stop the browser from submitting the form.
+  //   e.preventDefault();
 
-    // Serialize the form data.
-    var formData = $(form).serialize();
+  //   // Serialize the form data.
+  //   var formData = $(form).serialize();
 
-    // Submit the form using AJAX.
-    $.ajax({
-      type: 'POST',
-      url: $(form).attr('action'),
-      data: formData
-    })
-    .done(function(response) {
-      // Make sure that the formMessages div has the 'success' class.
-      $(formMessages).removeClass('error');
-      $(formMessages).addClass('success');
+  //   // Submit the form using AJAX.
+  //   $.ajax({
+  //     type: 'POST',
+  //     url: $(form).attr('action'),
+  //     data: formData
+  //   })
+  //   .done(function(response) {
+  //     // Make sure that the formMessages div has the 'success' class.
+  //     $(formMessages).removeClass('error');
+  //     $(formMessages).addClass('success');
 
-      // Set the message text.
-      $(formMessages).text('Email sent successfully.');
+  //     // Set the message text.
+  //     $(formMessages).text('Email sent successfully.');
 
-      // Clear the form.
-      $('#contact-form input,#contact-form textarea').val('');
-    })
-    .fail(function(data) {
-      // Make sure that the formMessages div has the 'error' class.
-      $(formMessages).removeClass('success');
-      $(formMessages).addClass('error');
+  //     // Clear the form.
+  //     $('#contact-form input,#contact-form textarea').val('');
+  //   })
+  //   .fail(function(data) {
+  //     // Make sure that the formMessages div has the 'error' class.
+  //     $(formMessages).removeClass('success');
+  //     $(formMessages).addClass('error');
 
-      // Set the message text.
-      if (data.responseText !== '') {
-        $(formMessages).text(data.responseText);
-      } else {
-        $(formMessages).text('Oops! An error occured and your message could not be sent.');
-      }
-    });
-  });
+  //     // Set the message text.
+  //     if (data.responseText !== '') {
+  //       $(formMessages).text(data.responseText);
+  //     } else {
+  //       $(formMessages).text('Oops! An error occured and your message could not be sent.');
+  //     }
+  //   });
+  // });
 
 });
 
